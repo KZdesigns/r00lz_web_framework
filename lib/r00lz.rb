e5ccc76f29a2cbc1cd46b9ae5073b41edd07a454
Kyle Zimmerman <kzdesignsios@gmail.com>
2025-02-16 11:22:52 -0800

    creates base app class for framework

--- lib/r00lz.rb
+++ lib/r00lz.rb
@@ -1,6 +1,11 @@
 require "r00lz/version"
 
 module R00lz
+  class App
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ['Hello from r00lz!']]
+    end
+  end
   class Error < StandardError; end
   # Your code goes here...
 end
